ead144df5eb548ff72255c862266b1e6f666ffb5
choibumsu <bumsu0211@gmail.com>
2020-10-11 14:31:01 +0900

    프로젝트 title, description, author 변경

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,11 +1,12 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Choi Teemo Devlog`,
+    description: `Frontend Developer Choi Teemo(@choibumsu) Devlog. JavaScript, TypeScript, SCSS, React, Vue.js, GraphQl, Prisma and All of Development`,
+    author: `@choibumsu`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-sass`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
