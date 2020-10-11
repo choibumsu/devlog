e903978449d70dda51302c6627d2b584ac49ddaf
choibumsu <bumsu0211@gmail.com>
2020-10-11 18:03:07 +0900

    favicon 변경

--- gatsby-config.js
+++ gatsby-config.js
@@ -24,13 +24,11 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `choi-teemo-devlog`,
+        short_name: `devlog`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/favicon.png`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
