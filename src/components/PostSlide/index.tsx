dd3606fb9b113809ee16522174fba7b210929754
choibumsu <bumsu0211@gmail.com>
2020-10-17 23:44:34 +0900

    포스트 리스트 컴포넌트 작성

---
+++ src/components/PostSlide/index.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react'
+import { Link } from 'gatsby'
+
+const IndexPage: React.FC = () => (
+  <div className="post-slide">
+    <Link to="/welcome">welcome</Link>
+  </div>
+)
+
+export default IndexPage
