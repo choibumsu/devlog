8f2b3efbbb8f3e573940d951a690aa67499e48a9
choibumsu <bumsu0211@gmail.com>
2020-12-05 23:47:21 +0900

    graphql 연결 시도

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,49 @@
 import * as React from 'react'
-import { Link } from 'gatsby'
-
+import { graphql, Link } from 'gatsby'
 import Layout from '../components/Layout'
 import Image from '../components/Image'
 import Seo from '../components/Seo'
 
-const IndexPage: React.FC = () => (
+type MarkdownProps = {
+  data: {
+    allMarkdownRemark: {
+      edges: [
+        node: {
+          frontmatter: {
+            title: string
+            date: Date
+            path: string
+          }
+          excerpt: string
+        }
+      ]
+    }
+  }
+}
+
+const IndexPage: React.FC<MarkdownProps> = ({ data }) => (
   <Layout>
     <Seo title="Home" />
-    <Link to="/welcome">welcome</Link>
+    <Link to="/welcome">{data}</Link>
+    <div>{data.allMarkdownRemark}</div>
   </Layout>
 )
 
 export default IndexPage
+
+export const postSummaryQuery = graphql`
+  query($fields: [MarkdownRemarkFieldsEnum], $order: [SortOrderEnum], $summaryLength: Int) {
+    allMarkdownRemark(sort: { fields: $fields, order: $order }) {
+      edges {
+        node {
+          frontmatter {
+            date(formatString: "YYYY년 MM월 DD일")
+            path
+            title
+          }
+          excerpt(pruneLength: $summaryLength)
+        }
+      }
+    }
+  }
+`
