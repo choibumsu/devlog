5a68cd043d81c7ecb10b6dfbfab67bbc86adb212
choibumsu <bumsu0211@gmail.com>
2021-05-02 23:41:36 +0900

    Graphql api 호출 테스트 롤백

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,49 +1,13 @@
 import * as React from 'react'
-import { graphql, Link } from 'gatsby'
+import { Link } from 'gatsby'
 import Layout from '../components/Layout'
 import Image from '../components/Image'
 import Seo from '../components/Seo'
 
-type MarkdownProps = {
-  data: {
-    allMarkdownRemark: {
-      edges: [
-        node: {
-          frontmatter: {
-            title: string
-            date: Date
-            path: string
-          }
-          excerpt: string
-        }
-      ]
-    }
-  }
-}
-
-const IndexPage: React.FC<MarkdownProps> = ({ data }) => (
+const IndexPage: React.FC = () => (
   <Layout>
     <Seo title="Home" />
-    <Link to="/welcome">{data}</Link>
-    <div>{data.allMarkdownRemark}</div>
   </Layout>
 )
 
 export default IndexPage
-
-export const postSummaryQuery = graphql`
-  query($fields: [MarkdownRemarkFieldsEnum], $order: [SortOrderEnum], $summaryLength: Int) {
-    allMarkdownRemark(sort: { fields: $fields, order: $order }) {
-      edges {
-        node {
-          frontmatter {
-            date(formatString: "YYYY년 MM월 DD일")
-            path
-            title
-          }
-          excerpt(pruneLength: $summaryLength)
-        }
-      }
-    }
-  }
-`
